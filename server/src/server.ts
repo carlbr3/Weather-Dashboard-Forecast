a41096a30b02325901b8a714564c0b2ea90b348d
carlbr3 <brandon.carlson2089@gmail.com>
2024-12-19 18:09:24 -0700

    update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,17 @@ dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
+import path from 'path';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static('client/dist'));
-
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.get ('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
